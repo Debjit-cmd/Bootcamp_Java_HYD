71d472edb0feeacbe3055faf56669b8353ee6821
Debjit Saha <124062703+Debjit-cmd@users.noreply.github.com>
2026-06-05 13:46:03 +0530

    Add files via upload

---
+++ BaseClass.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+public class BaseClass {
+	public void add() {
+		System.out.println("Addition will happen here");
+	}
+	public void subtract() {
+		System.out.println("Subtraction will happen here");
+	}
+}
---
+++ InheritanceClass.java
@@ -0,0 +1,7 @@
+package InheritancePackage;
+public class InheritanceClass {
+	public static void main(String[] args) {
+		
+	}
+
+}
